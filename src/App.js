e6c35a8f120aee26aec668bf70453ca2177247f9
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-23 00:25:54 +0530

    Add component Navbar and custom styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );

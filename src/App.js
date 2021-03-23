9921eb75f9826d33e39feb1be2bc087b9580cb52
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-23 14:16:43 +0530

    Add states and props to display the blog previews in homepage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
     <div className="App">
       <Navbar />
---
+++ src/BlogList.js
@@ -0,0 +1,14 @@
+const BlogList = (props) => {
+    return (
+        <div>
+            {props.blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;

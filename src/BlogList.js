940ae023f738c2f5fb093ab2a664f7c9ffb0e95b
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-23 21:33:37 +0530

    Add delete button to each blog in BlogList component

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,12 @@
-const BlogList = (props) => {
+const BlogList = ({ blogs, handleDelete }) => {
+
     return (
         <div>
-            {props.blogs.map(blog => (
+            {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import BlogList from './BlogList';
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -8,9 +8,14 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} />
+            <BlogList blogs={blogs} handleDelete={handleDelete} />
         </div>
     );
 }

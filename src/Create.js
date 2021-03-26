876e2edac22efc66e5e08b2814ec647fc195fddb
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-27 03:52:40 +0530

    Add handleSubmit function and fetch API to submit the new blog form

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,35 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const blog = { title, body, author };
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(blog),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        setTitle('');
+        setBody('');
+        setAuthor('mario');
+    }
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="blog-title">Blog Title:</label>
                 <input
                     id="blog-title"

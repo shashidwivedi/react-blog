548ecf901ea1b41bb2a4098574de8872f18c9de2
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-27 23:59:43 +0530

    Add Delete button in Blog Details component

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,29 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
 
     const { data: blog, isLoading, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                history.push('/');
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 
     return (
         <div className="blog-details">
@@ -15,6 +34,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
